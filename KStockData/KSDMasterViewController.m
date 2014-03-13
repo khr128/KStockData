254b43807d7362b4ef25c3e1f74fea9e10095040
Dmitri Khrebtukov <khr128@gmail.com>
2014-03-12 18:28:45 -0700

    wip: typo

--- KStockData/KSDMasterViewController.m
+++ KStockData/KSDMasterViewController.m
@@ -93,7 +93,7 @@
         [context deleteObject:[self.fetchedResultsController objectAtIndexPath:indexPath]];
         
         NSError *error = nil;
-        if (![context save:&error]) {s
+        if (![context save:&error]) {
              // Replace this implementation with code to handle the error appropriately.
              // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
             NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
