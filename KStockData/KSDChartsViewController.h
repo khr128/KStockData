175c18c1c766c29958c226f71ca48ca8a96d3daf
Dmitri Khrebtukov <khr128@gmail.com>
2014-03-20 16:48:21 -0700

    Add charts view controller subclass.

---
+++ KStockData/KSDChartsViewController.h
@@ -0,0 +1,13 @@
+//
+//  KSDChartsViewController.h
+//  KStockData
+//
+//  Created by khr on 3/20/14.
+//  Copyright (c) 2014 khr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KSDChartsViewController : UIViewController
+
+@end
