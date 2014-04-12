8a941082571ce733c5bae4a0bdb39f617fb678b8
Dmitri Khrebtukov <khr128@gmail.com>
2014-04-12 12:05:00 -0700

    Detect when stock already exists or symbol is invalid when adding new symbols.

--- KSDAddStockPopoverViewController.m
+++ KSDAddStockPopoverViewController.m
@@ -9,12 +9,16 @@
 #import "KSDAddStockPopoverViewController.h"
 #import "KSDMasterViewController.h"
 #import "KSDDetailViewController.h"
+#import "KSDStockDataRetriever.h"
+#import "NSString+NSString_KhrCSV.h"
 
 @interface KSDAddStockPopoverViewController ()
 
 @end
 
-@implementation KSDAddStockPopoverViewController
+@implementation KSDAddStockPopoverViewController {
+  KSDStockDataRetriever *_stockDataRetriever;
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -27,8 +31,8 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+  [super viewDidLoad];
+  _stockDataRetriever = [KSDStockDataRetriever new];
 }
 
 - (void)didReceiveMemoryWarning
@@ -41,33 +45,59 @@
 #pragma mark UITextfieldDelegate
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
-  NSManagedObjectContext *context = [self.masterViewController.fetchedResultsController managedObjectContext];
-  NSEntityDescription *entity = [[self.masterViewController.fetchedResultsController fetchRequest] entity];
-  NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:[entity name] inManagedObjectContext:context];
-  
-  // If appropriate, configure the new managed object.
-  // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
-  [newManagedObject setValue:[textField.text uppercaseString] forKey:@"symbol"];
-  
-  [newManagedObject setValue: [self.overBoughtSold titleForSegmentAtIndex:self.overBoughtSold.selectedSegmentIndex]
-                      forKey: @"watchType"];
-  
-  
-  // Save the context.
-  NSError *error = nil;
-  if (![context save:&error]) {
-    // Replace this implementation with code to handle the error appropriately.
-    // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-    NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-    abort();
-  }
-  
-  [self.presentingPopoverController dismissPopoverAnimated:YES];
-  self.masterViewController.detailViewController.detailItem = newManagedObject;
-  
-  NSIndexPath *rowIndexPath = self.masterViewController.tableView.indexPathForSelectedRow;
-  [self.masterViewController.tableView deselectRowAtIndexPath:rowIndexPath animated:YES];
-  
+  NSString *symbol = [textField.text uppercaseString];
+  [_stockDataRetriever stockDataFor:symbol
+                           commands:@"e1"
+                   completionHadler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                     NSString *csv = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+                     NSArray *array = [csv khr_csv];
+                     
+                     dispatch_queue_t mainQueue = dispatch_get_main_queue();
+                     if ([array[0] isEqualToString:@"N/A"]) {
+                       NSManagedObjectContext *context = [self.masterViewController.fetchedResultsController managedObjectContext];
+                       NSEntityDescription *entity = [[self.masterViewController.fetchedResultsController fetchRequest] entity];
+                       
+                       NSFetchRequest *fetchRequest = [NSFetchRequest new];
+                       fetchRequest.entity = entity;
+                       fetchRequest.predicate = [NSPredicate predicateWithFormat:@"symbol == %@", symbol];
+                       
+                       NSError *fetchError = nil;
+                       NSArray *fetchedData = [context executeFetchRequest:fetchRequest error:&fetchError];
+                       
+                       if (fetchedData.count < 1) {
+                          dispatch_async(mainQueue, ^{
+                           NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:[entity name] inManagedObjectContext:context];
+                           
+                           // If appropriate, configure the new managed object.
+                           // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
+                           [newManagedObject setValue:[textField.text uppercaseString] forKey:@"symbol"];
+                           
+                           [newManagedObject setValue: [self.overBoughtSold titleForSegmentAtIndex:self.overBoughtSold.selectedSegmentIndex]
+                                               forKey: @"watchType"];
+                           
+                           
+                           // Save the context.
+                           NSError *saveError = nil;
+                           if (![context save:&saveError]) {
+                             // Replace this implementation with code to handle the error appropriately.
+                             // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                             NSLog(@"Unresolved error %@, %@", error, [saveError userInfo]);
+                             abort();
+                           }
+                           
+                            self.masterViewController.detailViewController.detailItem = newManagedObject;
+                           
+                           NSIndexPath *rowIndexPath = self.masterViewController.tableView.indexPathForSelectedRow;
+                           [self.masterViewController.tableView deselectRowAtIndexPath:rowIndexPath animated:YES];
+                         });
+                       }
+                     }
+                     
+                     dispatch_async(mainQueue, ^{
+                       [self.presentingPopoverController dismissPopoverAnimated:YES];
+                     });
+                   }];
+
   return YES;
 }
 
